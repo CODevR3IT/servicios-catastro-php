120961d80d55b3740a459305ebf4fe23bfbdec26
Redd <razogarduno@gmail.com>
2021-02-19 18:20:27 -0600

    auth

--- app/Http/Controllers/BeneficiosController.php
+++ app/Http/Controllers/BeneficiosController.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
 use Validator;
 
 class BeneficiosController extends Controller
@@ -18,21 +20,32 @@ class BeneficiosController extends Controller
 
     public function getAplicados(Request $request)
     {
-        $validator = Validator::make($request->all(), [
-            'cuenta' => 'required|min:12|max:12',
-        ]);
+        
+            $validator = Validator::make($request->all(), [
+                'cuenta' => 'required|min:12|max:12',
+            ]);
 
-        //$authToken = $request->header('Authorization');
-        if ($validator->fails()) {
-            return response()->json(['mensaje' => 'Fallo en la petición, la cuenta es requerida con 12 caracteres.'], 400);
-        }
-        $cuenta = $request->input('cuenta');
-        $rfc = $request->input('rfc') ? $request->input('rfc'): '';
-        $curp = $request->input('curp') ? $request->input('curp'): '';
-        $mensaje = '';
-
-        try {
+            $authToken = $request->header('Authorization');
+            try{
+                $auth = Crypt::decrypt($authToken);
+            }catch(\Throwable $th){
+                return response()->json(['mensaje' => 'Se requiere el token de autorización'], 403);
+            }
             
+            if(!$auth['logged']){
+                return response()->json(['mensaje' => 'No tiene permisos para acceder a la API'], 403);
+            }
+
+            if ($validator->fails()) {
+                return response()->json(['mensaje' => 'Fallo en la petición, la cuenta es requerida con 12 caracteres.'], 400);
+            }
+            $cuenta = $request->input('cuenta');
+            $rfc = $request->input('rfc') ? $request->input('rfc') : '';
+            $curp = $request->input('curp') ? $request->input('curp') : '';
+            $mensaje = '';
+
+
+            try {
             $procedure = 'BEGIN 
             fis.fis_consultas_ws.fis_obt_beneficios_aplicados_p(
                 :par_cuenta,
@@ -57,10 +70,10 @@ class BeneficiosController extends Controller
             oci_close($conn);
             oci_fetch_all($cursor, $beneficios, 0, -1, OCI_FETCHSTATEMENT_BY_ROW + OCI_ASSOC);
             oci_free_cursor($cursor);
-           
-            if(!empty($beneficios)){
+
+            if (!empty($beneficios)) {
                 return response()->json($beneficios[0], 200);
-            }else{
+            } else {
                 return response()->json(['mensaje' => $mensaje], 404);
             }
         } catch (\Throwable $th) {
