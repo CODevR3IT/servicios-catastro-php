70961b5f9b9757e039d3261523072a80df97bcc3
Redd <razogarduno@gmail.com>
2021-04-12 20:48:01 -0500

    env y database config

--- config/database.php
+++ config/database.php
@@ -14,6 +14,19 @@ return [
             'prefix'        => env('DB_PREFIX', ''),
             'prefix_schema' => env('DB_SCHEMA_PREFIX', ''),
             'edition'       => env('DB_EDITION', 'ora$base'),
-        ]
+        ],
+        'ws' => [
+            'driver'        => 'oracle',
+            'tns'           => env('DB_TNS_PRECAT', ''),
+            'host'          => env('DB_HOST_PRECAT', ''),
+            'port'          => env('DB_PORT', '1521'),
+            'database'      => env('DB_DATABASE_PRECAT', ''),
+            'username'      => env('DB_USERNAME_PRECAT', ''),
+            'password'      => env('DB_PASSWORD_PRECAT', ''),
+            'charset'       => env('DB_CHARSET', 'AL32UTF8'),
+            'prefix'        => env('DB_PREFIX', ''),
+            'prefix_schema' => env('DB_SCHEMA_PREFIX', ''),
+            'edition'       => env('DB_EDITION', 'ora$base'),
+        ],
     ]
         ];
